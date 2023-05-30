014e0cef5719ec82d890f78e368e9e4cf6f1e442
Chutengli <72440194+Chutengli@users.noreply.github.com>
2023-05-29 17:59:20 -0700

    fixed bug: thread concurrency

--- Server/T3Server.java
+++ Server/T3Server.java
@@ -20,75 +20,110 @@ public class T3Server {
     public static void main(String... args) {
         PORT = args.length == 1 ? Integer.parseInt(args[0]) : 31161;
         games = new HashMap<>();
-        executorService.submit(T3Server::tcp);
-        executorService.submit(T3Server::udp);
+
+        try {
+            ServerSocket tcpSocket = new ServerSocket(PORT);
+            DatagramSocket udpSocket = new DatagramSocket(PORT);
+
+            new Thread(() -> {
+                try {
+                    System.out.println("TCP: Server is listening on port " + PORT);
+
+                    while (true) {
+                        Socket socket = tcpSocket.accept();
+
+                        executorService.submit(() -> {
+                            try {
+                                tcp(socket);
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        });
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }).start();
+
+            new Thread(() -> {
+                System.out.println("UDP: Server is listening on port " + PORT);
+
+                DatagramPacket request = new DatagramPacket(new byte[512], new byte[512].length);
+                while (true) {
+                    try {
+                        udpSocket.receive(request);
+                        executorService.submit(() -> {
+                            udp(udpSocket, request);
+                        });
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }).start();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private static void tcp() {
-        System.out.println("TCP: Server is listening on port " + PORT);
-        try (ServerSocket server = new ServerSocket(PORT)) {
-            Socket socket = server.accept();
-            System.out.println("TCP: Received TCP request");
-            while(socket != null) {
-                // read client command
-                InputStream in = socket.getInputStream();
-                OutputStream out = socket.getOutputStream();
+    private static void tcp(Socket socket) throws IOException {
+        System.out.println("TCP: Received TCP request");
+        while(socket != null) {
+            // read client command
+            InputStream in = socket.getInputStream();
+            OutputStream out = socket.getOutputStream();
 
-                String clientRequest = readClient(in);
-                System.out.println("received client request: \n" + clientRequest);
-                if(clientRequest == null) {
-                    sendResponse("bad request, please try again", out);
-                    socket.close();
-                    return ;
-                }
+            String clientRequest = readClient(in);
+            System.out.println("received client request: \n" + clientRequest);
+            if(clientRequest == null) {
+                sendResponse("bad request, please try again", out);
+                socket.close();
+                return ;
+            }
 
-                JSONObject clientReqJson = new JSONObject(clientRequest);
-                // handle command
-                String sessionID;
-                switch (clientReqJson.getString("command")) {
-                    case "HELO":
-                        sessionID = generateRandomString();
-                        sendResponse("SESS " + sessionID + " " + clientReqJson.getString("clientID") + "\n\r", out);
-                        break;
-                    case "CREA":
-                        // "JOND " CID " " + GID <- return format needs to be like this
-                        String gid = generateRandomString();
-                        GameState newGame = new GameState();
-                        // find a way to get the player name and playerID
-                        String playerID = clientReqJson.getString("clientID");
-                        newGame.join("Jason", playerID);
-                        games.put(gid, newGame);
-                        sendResponse("JOND " + gid + " " + games.get(gid) + "\n\r", out);
-                        break;
-                    case "LIST":
-                        sendResponse("GAMS " + getGames(), out);
-                        break;
-                    case "JOIN": // join a given game
-                        String clientSentGID = "";
-                        sendResponse("JOND " + games.get(clientSentGID), out); // "JOND " CID " " + GID <- return format needs to be like thi
-                        break;
-                    case "QUIT":
+            JSONObject clientReqJson = new JSONObject(clientRequest);
+            // handle command
+            String sessionID;
+            switch (clientReqJson.getString("command")) {
+                case "HELO":
+                    sessionID = generateRandomString();
+                    sendResponse("SESS " + sessionID + " " + clientReqJson.getString("clientID") + "\n\r", out);
+                    break;
+                case "CREA":
+                    // "JOND " CID " " + GID <- return format needs to be like this
+                    String gid = generateRandomString();
+                    GameState newGame = new GameState();
+                    // find a way to get the player name and playerID
+                    String playerID = clientReqJson.getString("clientID");
+                    newGame.join("Jason", playerID);
+                    games.put(gid, newGame);
+                    sendResponse("JOND " + gid + " " + games.get(gid) + "\n\r", out);
+                    break;
+                case "LIST":
+                    sendResponse("GAMS " + getGames(), out);
+                    break;
+                case "JOIN": // join a given game
+                    String clientSentGID = "";
+                    sendResponse("JOND " + games.get(clientSentGID), out); // "JOND " CID " " + GID <- return format needs to be like thi
+                    break;
+                case "QUIT":
 //                        get game ID from client
 //                        String gid
 //                        sendResponse(out, gid);
-                        socket.close();
-                        break;
-                    case "STAT":
-                        break;
-                    case "MOV":
-                        break;
-                    case "GDBY":
-                        socket.close();
-                        break;
-                    default:
-                        sendResponse("command does not exist", out);
-                        socket.close();
-                        break;
-                }
+                    socket.close();
+                    break;
+                case "STAT":
+                    break;
+                case "MOV":
+                    break;
+                case "GDBY":
+                    socket.close();
+                    break;
+                default:
+                    sendResponse("command does not exist", out);
+                    socket.close();
+                    break;
             }
-        }
-        catch(Exception ex) {
-            ex.printStackTrace();
         }
     }
 
@@ -162,12 +197,9 @@ public class T3Server {
         return null;
     }
 
-    private static void udp() {
-        System.out.println("UDP: Server is listening on port " + PORT);
+    private static void udp(DatagramSocket socket, DatagramPacket request) {
         try {
             while (true) {
-                DatagramSocket socket = new DatagramSocket(PORT);
-                DatagramPacket request = new DatagramPacket(new byte[512], 1);
                 socket.receive(request);
                 System.out.println("UDP: Received UDP request");
                 InetAddress clientAddress = request.getAddress();
